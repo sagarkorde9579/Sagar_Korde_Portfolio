d2931566db7d608cf51793f22f823c653e73d59e
Sagar Sukhdev Korde <sk3397485@gmail.com>
2026-03-11 13:19:33 +0530

    Update App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,29 +1,3 @@
-// import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-// import Navbar from "./components/Navbar/Navbar";
-// import Projects from "./pages/Projects/Projects";
-// import Skills from "./pages/Skills/Skills";
-// import Contact from "./pages/Contact/Contact";
-// import Home from "./pages/Home/Home";
-// import Resume from "./pages/Resume/Resume";
-// import About from "./pages/About/About";
-
-// function App() {
-//   return (
-//     <Router>
-//       <Navbar />
-//       <Routes>
-//         <Route path="/" element={<Home />} />
-//         <Route path="/about" element={<About />} />
-//         <Route path="/skills" element={<Skills />} />
-//         <Route path="/projects" element={<Projects />} />
-//         <Route path="/contact" element={<Contact />} />
-//        <Route path="/resume" element={<Resume/>}/>
-//       </Routes>
-//     </Router>
-//   );
-// }
-
-// export default App;
 import { Routes, Route } from "react-router-dom";
 import Navbar from "./components/Navbar/Navbar";
 
